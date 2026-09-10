4c30120a3b2b98bdde660a1745d5ce49c72f7e15
yunyj1998 <yunyj1998@gmail.com>
2026-09-10 11:19:36 +0900

    fix: auto patch real d1 id after build

---
+++ patch-db.js
@@ -0,0 +1,8 @@
+﻿const fs = require('fs');
+const file = 'dist/server/wrangler.json';
+if (fs.existsSync(file)) {
+  let content = fs.readFileSync(file, 'utf8');
+  content = content.replace(/00000000-0000-4000-8000-000000000000/g, '92d656ef-b539-4904-8331-37cda3a8dbc1');
+  fs.writeFileSync(file, content);
+  console.log('Successfully patched D1 database ID!');
+}
